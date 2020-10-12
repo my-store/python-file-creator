c151142535471373b70813e1db0eaee84cf9b4af
Izzat alharis <49787648+my-store@users.noreply.github.com>
2020-10-12 20:27:55 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,24 +1,38 @@
-import mysql.connector as mysql
 
-class Connection :
-    def __init__(self) :
-        self.db         = mysql.connect(
-            host        = "localhost",
-            user        = "root",
-            password    = "rombax212",
-            database    = "rombax_login"
-        )
-        self.crsr()
+import os
 
-    def crsr(self) :
-        self.result = self.db.cursor()
+credit = """
+|
+|   FILE CREATOR - ROMBAX FAMILY BREBES JAWA TENGAH
+|
+|   Creator  : Izzat alharis
+|   Phone    : (+62) 813-9355-2220
+|   Email    : izzatalharis@gmail.com
+|   Github   : https://github.com/my-store
+|"""
 
-    def exc(self, p) :
-        self.result.execute(p)
+class NggaweFile :
+    def __init__(NF,Aran_Folder,Aran_File,Type_File) :
+        NF.Aran     = Aran_File
+        NF.Folder   = Aran_Folder
+        NF.Tipe     = Type_File
+        NF.Mandor   = credit
+        NF.gawe(NF.Tipe)
 
-p1 = Connection()
+    def gawe(NF,fType) :
+        if not os.path.isdir(NF.Folder) :
+            os.mkdir(NF.Folder)
 
-p1.exc("SHOW TABLES")
+        NF.File = open(NF.Folder+"/"+NF.Aran+"."+fType,"w")
+        NF.tulis(fType)
+        NF.File.close()
+        print("File `"+NF.Aran+"."+fType+"` teng folder `"+NF.Folder+"` sampun dados!")
 
-for x in p1.result :
-    print(x)
+    def tulis(NF,fType) :
+        NF.File.write("\n/*\n"+NF.Mandor+"\n*/\n\n")
+
+print("\n===== FILE CREATOR ======\n")
+namaFile = input("Silahkan isi nama file : ")
+jenisFile = input("Silahkan isi jenis file : ")
+namaFolder = input("Silahkan isi nama folder : ")
+NggaweFile(namaFolder,namaFile,jenisFile)
